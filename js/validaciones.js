105a8a99a8933c1c3bc512fe003e17fc59a2ccfe
alexabadp <aalex.abad96@gmail.com>
2023-03-31 22:53:21 -0500

    atributos HTML - validadores

---
+++ js/validaciones.js
@@ -0,0 +1,35 @@
+export function valida(input) {
+  const tipoDeInput = input.dataset.tipo;
+  if (validadores[tipoDeInput]) {
+    validadores[tipoDeInput](input);
+  }
+}
+
+const validadores = {
+  nacimiento: (input) => validarNacimiento(input),
+};
+
+const inputNacimiento = document.querySelector("#birth");
+
+inputNacimiento.addEventListener("blur", (event) => {
+  validarNacimiento(event.target);
+});
+
+function validarNacimiento(input) {
+  const fechaCliente = new Date(input.value);
+  let mensaje = "";
+  if (!mayorDeEdad(fechaCliente)) {
+    mensaje = "Debes tener al menos 18 años";
+  }
+  input.setCustomValidity(mensaje);
+}
+
+function mayorDeEdad(fecha) {
+  const fechaActual = new Date();
+  const diferenciaFechas = new Date(
+    fecha.getUTCFullYear() + 18,
+    fecha.getUTCMonth(),
+    fecha.getUTCDate()
+  );
+  return diferenciaFechas <= fechaActual;
+}
